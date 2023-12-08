59e5b038346f19877222523081a4807d1aab6549
npark080 <npark080@gmail.com>
2023-12-07 20:34:37 -0600

    Completed framework for program

--- Dungeon/Program.cs
+++ Dungeon/Program.cs
@@ -1,2 +1,82 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Dungeon
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            #region Title/Introduction
+
+            //TODO add title & intro
+
+            #endregion
+
+            #region Variable to Keep Score
+
+            //TODO add a variable to keep score
+
+            #endregion
+
+            #region Weapon Object Creation
+
+            //TODO add weapons
+
+            #endregion
+
+            #region Player Object Creation & Customization
+
+            //TODO create player
+            //name, race, stats
+
+            #endregion
+
+            #region Main Game Loop
+            bool exit = false;
+
+            do
+            {
+                #region MonsterObject Creation
+
+                //TODO Create monsters
+
+                #endregion
+
+                #region Room Creation
+
+                //TODO create rooms
+
+                #endregion
+
+                #region Gameplay Menu Loop
+
+                bool reload = false;
+
+                do
+                {
+                    #region Display the Menu
+
+                    //TODO Display the menu to the user
+
+                    #endregion
+
+                    #region Check Player Life
+
+                    //TODO check player life and kill if <= 0
+
+                    #endregion
+
+                } while (!exit && !reload);
+
+                #endregion
+
+            } while (!exit);
+
+            #endregion
+
+            #region Output Final Score / End the Game
+
+            //TODO show score
+
+            #endregion
+        }
+    }
+}
